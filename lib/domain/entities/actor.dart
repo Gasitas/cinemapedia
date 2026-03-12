09cc9499e93f2312f3f376cbcb0533a0f70f16e4
Gasitas <enriquedz@gmail.com>
2026-03-12 07:51:43 +0100

    Implementing actors

---
+++ lib/domain/entities/actor.dart
@@ -0,0 +1,11 @@
+class Actor {
+  final String name;
+  final int age;
+  final String nationality;
+
+  Actor({
+    required this.name,
+    required this.age,
+    required this.nationality,
+  });
+}
